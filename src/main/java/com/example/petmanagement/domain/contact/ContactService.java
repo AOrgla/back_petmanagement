6404f66652ba8993fa205ceda8e67246bb0728fa
Andres Orgla <aorgla@gmail.com>
2022-12-23 22:13:09 +0200

    Added validation for username, email

--- src/main/java/com/example/petmanagement/domain/contact/ContactService.java
+++ src/main/java/com/example/petmanagement/domain/contact/ContactService.java
@@ -1,10 +1,7 @@
 package com.example.petmanagement.domain.contact;
 
 
-import com.example.petmanagement.domain.contact.Contact;
-import com.example.petmanagement.domain.contact.ContactRepository;
 import com.example.petmanagement.validation.Validation;
-import org.apache.commons.lang3.Validate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
